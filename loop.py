2774ece7aa7b603d50ad4baaef00836ac1d9b59a
mohdasharshakeel <mohdasharshakeel@gmail.com>
2026-01-23 00:02:16 +0530

    loops

---
+++ loop.py
@@ -0,0 +1,17 @@
+
+#! Basic for loop (range ke sath)
+# for i in range(5):
+#     print(i)
+#! Basic while loop (range ke sath)
+
+# i = 1
+# while i <= 10:
+#     print(i)
+#     i+=1
+
+a = range(5,0,-1)
+
+for i in a : 
+    print(i)
+
+    
