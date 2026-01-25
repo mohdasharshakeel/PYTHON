509f802128afde6334e6c01f7e28de16d8485297
mohdasharshakeel <mohdasharshakeel@gmail.com>
2026-01-25 09:55:50 +0530

    table printing

--- loop.py
+++ loop.py
@@ -9,12 +9,23 @@
 #     print(i)
 #     i+=1
 
-a = range(5,0,-1)
+# a = range(5,0,-1)
 
-for i in a : 
-    print(i)
+# for i in a : 
+#     print(i)
 
     
-for i in range(1, 11):
-    if i % 2 == 0:
-        print(i, "even number")
+# for i in range(1, 11):
+#     if i % 2 == 0:
+#         print(i, "even number")
+
+#Table print 
+
+# for i in range(6,61,6):
+#     print(i)
+
+#! table with input
+
+n = int(input("Enter the number of table :"))
+for i in range(n,n*10+1,n):
+    print(i)
