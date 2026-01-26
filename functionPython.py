1d1e8e995ddd2f52f879bb28c7d398daf3c41152
mohdasharshakeel <mohdasharshakeel@gmail.com>
2026-01-26 21:51:54 +0530

    pallindrom

--- functionPython.py
+++ functionPython.py
@@ -5,3 +5,14 @@ sayName("Ashar")
 def arg(a,b=10):
     print(a + b)
 arg(10,50)
+
+def pallindrom(st):
+    rev = ""
+    for i in range(len(st)-1,-1,-1):
+        rev = rev + st[i]
+    if rev == st:
+        print("pallindrome")
+    else:
+        print("not a pallindrom")
+pallindrom("HIHIH")
+pallindrom("Ashar")
