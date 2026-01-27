ecd3228828a826e53e0e85b5e2834e0806db4ced
mohdasharshakeel <mohdasharshakeel@gmail.com>
2026-01-27 09:58:05 +0530

    second largest

--- listPython.py
+++ listPython.py
@@ -11,12 +11,26 @@
 # a.remove(15)#? remove kar dega 
 
 
-k = [-2,32,12,44,-12]
-for i in range(len(k)):
-    if k[i] >= 0:
-        print(k[i])
-for i in k:
-    if i < 0:
-        print(i)
+# k = [-2,32,12,44,-12]
+# for i in range(len(k)):
+#     if k[i] >= 0:
+#         print(k[i])
+# for i in k:
+#     if i < 0:
+#         print(i)
+
+#! Second largest value
+l = [12,13,16,19,17]
+largest = l[0]
+secondLarest = l[0]
+
+for i in range(len(l)):
+    if l[i] > largest:
+        secondLarest = largest 
+        largest = l[i]
+    elif l[i] > secondLarest:
+        secondLarest = l[i]
+print(secondLarest, largest)
+
 
 
