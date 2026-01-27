a3695b74cec14aca14dd1f44a4f18c0b8b3b66bf
mohdasharshakeel <mohdasharshakeel@gmail.com>
2026-01-27 09:37:05 +0530

    list python

---
+++ listPython.py
@@ -0,0 +1,11 @@
+a = [12,14,15,16]
+for i in range(len(a)):
+    print(a[i])
+for i in a:
+    print(i)
+
+#! Methods of list
+
+a.append(17) #? Last me jodo
+a.insert(1,13) #? Kisi bhi position per
+a.remove(15)#? remove kar dega 
